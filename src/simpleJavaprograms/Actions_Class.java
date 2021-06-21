44865a1929bf999be8610db15e2761ea9a32486a
ASUS <ASUS@LAPTOP-H29TJ6C2>
2021-06-21 23:20:02 +0530

    second commit

---
+++ src/simpleJavaprograms/Actions_Class.java
@@ -0,0 +1,16 @@
+package simpleJavaprograms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Actions_Class {
+	
+	
+	public void ActionMethod() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\Desktop\\Selenium Driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
